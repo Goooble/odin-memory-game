618c9669cab12ee0c500156faf15d66310010f08
Goooble <bivinbheemaiah@gmail.com>
2025-10-21 18:14:16 +0530

    bug: Fix game win green tiles

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,8 +1,17 @@
 import "./css/card.css";
 
-function Card({ id, setScore, score, isSelected, setSelected, pokemon, isWon, setWon }) {
+function Card({
+  id,
+  setScore,
+  score,
+  isSelected,
+  setSelected,
+  pokemon,
+  isWon,
+  setWon,
+}) {
   function handleClick() {
-    console.log(score)
+    console.log(score);
     if (isSelected[id]) {
       setScore(0);
       setSelected(new Array(10).fill(false));
@@ -12,16 +21,14 @@ function Card({ id, setScore, score, isSelected, setSelected, pokemon, isWon, se
       array[id] = true;
       setSelected(array);
     }
-    if(score===10){
-        console.log("jel")
-        setWon(isWon+1);
-        
+    if (score === 10) {
+      console.log("jel");
+      setWon(isWon + 1);
     }
-    
   }
-  let className = "card " + isSelected[id];
-  score === 10 ? (className += " won") : null;
-  
+  let className = "card " //+ isSelected[id];//to test the game visual reference
+  score === 10 && (className += " won");
+
   return (
     <button onClick={handleClick} className={className}>
       {pokemon.path ? <img src={pokemon.path} /> : <p>Loading...</p>}
